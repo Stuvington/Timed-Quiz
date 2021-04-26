4379d3aaa9d6855b308ed9691e662b1913fcd56d
Stuvington <Lord.Stuvington@gmail.com>
2021-04-26 00:29:02 -0400

    [Basics] Nothing functional yet

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// a doot doot doo
+
+// global variables
+// new Object()
+let result = new Object()
+result.first = [`1`, `AL`, `123`]
+
+document.getElementById("result.first")
+
+console.log(result.first)
+console.log(result.first[1])
+
+
+
+// functional definitions
+
+
+
+
+// event listeners and invocations
